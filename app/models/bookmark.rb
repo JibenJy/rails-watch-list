f5736e6529d45f32093774cc6b68cb2fe80a1993
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-12 17:50:08 +0200

    web

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,9 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: {minimum: 6}, allow_blank: true
+  validates :comment, length: {minimum: 6}
   validates :comment, presence: true
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates :list, uniqueness: {scope: :movie}
 end
