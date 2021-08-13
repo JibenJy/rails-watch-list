d7e9047479e0a6c9a301c942b1f84eddf0689a95
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-13 13:09:10 +0200

    cloudinary my man

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,10 @@
 class List < ApplicationRecord
     has_many :bookmarks, dependent: :destroy
+    has_one_attached :photo
+
     has_many :movies, through: :bookmarks
+
+    validates :photo, presence: true
     validates :name, presence: true
     validates :name, uniqueness: true
     
